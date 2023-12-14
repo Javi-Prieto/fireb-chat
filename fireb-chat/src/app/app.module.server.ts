263208ce2a6f7dab9cd6c61c97eb81cd3fd64dd5
Javier Prieto <jpojaviprior@gmail.com>
2023-12-14 23:33:40 +0100

    added base config for firebase

--- fireb-chat/src/app/app.module.server.ts
+++ fireb-chat/src/app/app.module.server.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   bootstrap: [AppComponent],
 })
--- fireb-chat/src/environments/environment.development.ts
+++ fireb-chat/src/environments/environment.development.ts
@@ -1,3 +1,22 @@
 export const environment = {
-    production: false
+    production: false,
+    firebaseConfig : {
+
+        apiKey: "AIzaSyDXDnKX0wOJMY9HP34ZJQ5NEj3HvcjaLok",
+      
+        authDomain: "fireb-chat-3784e.firebaseapp.com",
+      
+        databaseURL: "https://fireb-chat-3784e-default-rtdb.europe-west1.firebasedatabase.app",
+      
+        projectId: "fireb-chat-3784e",
+      
+        storageBucket: "fireb-chat-3784e.appspot.com",
+      
+        messagingSenderId: "491140473449",
+      
+        appId: "1:491140473449:web:f7b028cace939adde9b8f8",
+      
+        measurementId: "G-4SGR3DDEPQ"
+      
+      }
 };
